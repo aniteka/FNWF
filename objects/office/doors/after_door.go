cd3dec58b50fbddbe693aff8f7ddb19c933c1413
Miles <derpytimota4ka@gmail.com>
2022-06-28 12:17:37 +0100

    Add camera-office changing. Add camera preview.

--- objects/office/doors/after_door.go
+++ objects/office/doors/after_door.go
@@ -1,9 +1,24 @@
 components {
-  id: "left_door"
-  component: "/objects/office/doors/left_door.sprite"
+  id: "door_script"
+  component: "/objects/office/doors/door_script.script"
   position {
     x: 0.0
     y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "left_door"
+  component: "/objects/office/doors/left_door.sprite"
+  position {
+    x: 41.931
+    y: 260.0
     z: 0.0
   }
   rotation {
@@ -17,23 +32,8 @@ components {
   id: "right_door"
   component: "/objects/office/doors/right_door.sprite"
   position {
-    x: 10080.0
-    y: -4.411
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
-components {
-  id: "door_script"
-  component: "/objects/office/doors/door_script.script"
-  position {
-    x: 0.0
-    y: 0.0
+    x: 7441.008
+    y: 260.0
     z: 0.0
   }
   rotation {
