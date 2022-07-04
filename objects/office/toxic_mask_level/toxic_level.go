f37e30aa1700089f38e3d6b414edfb4c22b63552
Miles <derpytimota4ka@gmail.com>
2022-07-04 11:46:35 +0100

    Add toxic level

---
+++ objects/office/toxic_mask_level/toxic_level.go
@@ -0,0 +1,15 @@
+components {
+  id: "toxic_level"
+  component: "/objects/office/toxic_mask_level/toxic_level.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
